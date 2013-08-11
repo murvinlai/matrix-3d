426cb3d6d8c3feb58d8e164a9d46247c5a8b6fb5
Murvin Lai <murvinlai@gmail.com>
2013-08-11 06:24:57 -0400

    fix typo

--- test/test-basic.js
+++ test/test-basic.js
@@ -6,15 +6,15 @@ var x = 10;
 var y = 2;
 var z = -5;
 
-var m = matrix3d.tranlate_x(x);
+var m = matrix3d.translate_x(x);
 console.log("Tx: " + JSON.stringify(m));
 assert.equal(m[0][3], x);
 
-var m = matrix3d.tranlate_y(y);
+var m = matrix3d.translate_y(y);
 console.log("Ty: " + JSON.stringify(m));
 assert.equal(m[1][3], y);
 
-var m = matrix3d.tranlate_z(z);
+var m = matrix3d.translate_z(z);
 console.log("Tz: " + JSON.stringify(m));
 assert.equal(m[2][3], z);
 
